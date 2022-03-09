5a5aeb767e241458dc6bc19db2808e415ae14a13
Diogo Pereira <diogopereira@MacBook-Air-de-Diogo.local>
2022-03-09 15:13:58 +0000

    2nd try

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jguerrei <jguerrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 14:24:28 by jguerrei          #+#    #+#             */
-/*   Updated: 2022/02/02 15:33:52 by jguerrei         ###   ########.fr       */
+/*   Updated: 2022/03/09 15:11:48 by jguerrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!dst || !src)
-		return (0);
+	if (dst == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
